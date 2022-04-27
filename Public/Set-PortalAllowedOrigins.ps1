d73a6280e792473df3236a1e407ac278e73d8408
jjohns <jjohns@esri.com>
2022-04-27 09:10:22 -0700

    Added SkipCertificateCheck

--- Public/Set-PortalAllowedOrigins.ps1
+++ Public/Set-PortalAllowedOrigins.ps1
@@ -10,6 +10,8 @@ function Set-PortalAllowedOrigins {
         Origin to be allowed for Portal
     .PARAMETER Token
         Portal token
+    .PARAMETER SkipCertificateCheck
+        Ignore missing or invalid certificate
     .INPUTS
         None.
     .OUTPUTS
@@ -32,7 +34,10 @@ function Set-PortalAllowedOrigins {
 
         [Parameter(Mandatory, HelpMessage = 'Portal token')]
         [ValidateScript({ $_ -match $token_regex })]
-        [System.String] $Token
+        [System.String] $Token,
+
+        [Parameter(HelpMessage = 'Skip SSL certificate check')]
+        [System.Management.Automation.SwitchParameter] $SkipCertificateCheck
     )
     Process {
         $restParams = @{
@@ -44,6 +49,7 @@ function Set-PortalAllowedOrigins {
                 allowedOrigins = ($Origin -join ',')
             }
         }
+        if ($PSBoundParameters.ContainsKey('SkipCertificateCheck')) { $restParams['SkipCertificateCheck'] = $true }
         Invoke-RestMethod @restParams
     }
 }
