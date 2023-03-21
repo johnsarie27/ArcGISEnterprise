c07e1bab72e2b0a90aeb8cae7bbad94ff17fd4ac
jjohns <jjohns@esri.com>
2023-03-21 08:56:27 -0700

    Removed raw query code as it does not work

--- Public/Search-PortalContent.ps1
+++ Public/Search-PortalContent.ps1
@@ -14,8 +14,6 @@ function Search-PortalContent {
         Type
     .PARAMETER Title
         Title
-    .PARAMETER Query
-        Key word(s) to query
     .PARAMETER Context
         Portal context (e.g., https://arcgis.com/arcgis)
     .PARAMETER Token
@@ -32,41 +30,36 @@ function Search-PortalContent {
     .NOTES
         Name:     Search-PortalContent
         Author:   Justin Johns
-        Version:  0.1.0 | Last Edit: 2023-03-20
+        Version:  0.1.1 | Last Edit: 2023-03-21
+        - 0.1.1 - Removed raw query
         - 0.1.0 - Initial version
         Comments: <Comment(s)>
         General notes
         https://developers.arcgis.com/rest/users-groups-and-items/search-reference.htm
         This function does not yet support multiple search values for a given criterion
         (e.g., can only search for a single "tag" not two tags)
-
-        The "Query" parameter does not work at this time. Needs fixing.
     #>
-    [CmdletBinding(DefaultParameterSetName = '__query')]
+    [CmdletBinding()]
     Param(
-        [Parameter(Mandatory = $false, HelpMessage = 'Id', ParameterSetName = '__query')]
+        [Parameter(Mandatory = $false, HelpMessage = 'Id')]
         [ValidateNotNullOrEmpty()]
         [System.String] $Id,
 
-        [Parameter(Mandatory = $false, HelpMessage = 'Owner', ParameterSetName = '__query')]
+        [Parameter(Mandatory = $false, HelpMessage = 'Owner')]
         [ValidateNotNullOrEmpty()]
         [System.String] $Owner,
 
-        [Parameter(Mandatory = $false, HelpMessage = 'Tag', ParameterSetName = '__query')]
+        [Parameter(Mandatory = $false, HelpMessage = 'Tag')]
         [ValidateNotNullOrEmpty()]
         [System.String] $Tag,
 
-        [Parameter(Mandatory = $false, HelpMessage = 'Type', ParameterSetName = '__query')]
+        [Parameter(Mandatory = $false, HelpMessage = 'Type')]
         [ValidateNotNullOrEmpty()]
         [System.String] $Type,
 
-        [Parameter(Mandatory = $false, HelpMessage = 'Title', ParameterSetName = '__query')]
+        [Parameter(Mandatory = $false, HelpMessage = 'Title')]
         [ValidateNotNullOrEmpty()]
         [System.String] $Title,
-
-        <# [Parameter(Mandatory = $true, HelpMessage = 'Key word(s) to query', ParameterSetName = '__keyword')]
-        [ValidateNotNullOrEmpty()]
-        [System.String] $Query, #>
 
         [Parameter(Mandatory = $true, HelpMessage = 'Target Portal context')]
         [ValidateScript({ $_.AbsoluteUri -match $context_regex })]
@@ -93,51 +86,45 @@ function Search-PortalContent {
             Body   = @{ f = 'json'; token = $Token }
         }
 
-        # PARAMETER SET
-        if ($PSCmdlet.ParameterSetName -EQ '__keyword') {
-            $restParams.Body['q'] = $Query
+        # CHECK FOR NO INPUT
+        if (-NOT $Id -AND -NOT $Owner -AND -NOT $Tag -AND -NOT $Type -AND -NOT $Title) {
+            Throw 'No seach parameter found. Please add search parameter and try again.'
         }
-        else {
-            # CHECK FOR NO INPUT
-            if (-NOT $Id -AND -NOT $Owner -AND -NOT $Tag -AND -NOT $Type -AND -NOT $Title) {
-                Throw 'No seach parameter found. Please add search parameter and try again.'
-            }
 
-            # ADD SEARCH PARAMS
-            switch ($PSBoundParameters.Keys) {
-                'Id' {
-                    $restParams.Body['q'] = 'id:"{0}"' -f $Id
-                    $searchParams = 1
-                }
-                'Owner' {
-                    if ($searchParams) { $restParams.Body['q'] += ' AND owner:"{0}"' -f $Owner }
-                    else { $restParams.Body['q'] = 'owner:"{0}"' -f $Owner }
-                    $searchParams++
-                }
-                'Tag' {
-                    if ($searchParams) { $restParams.Body['q'] += ' AND tags:"{0}"' -f $Tag }
-                    else { $restParams.Body['q'] = 'tags:"{0}"' -f $Tag }
-                    $searchParams++
-                }
-                'Type' {
-                    if ($searchParams) { $restParams.Body['q'] += ' AND type:"{0}"' -f $Type }
-                    else { $restParams.Body['q'] = 'type:"{0}"' -f $Type }
-                    $searchParams++
-                }
-                'Title' {
-                    if ($searchParams) { $restParams.Body['q'] += ' AND title:"{0}"' -f $Title }
-                    else { $restParams.Body['q'] = 'title:"{0}"' -f $Title }
-                    $searchParams++
-                }
+        # ADD SEARCH PARAMS
+        switch ($PSBoundParameters.Keys) {
+            'Id' {
+                $restParams.Body['q'] = 'id:"{0}"' -f $Id
+                $searchParams = 1
             }
-
-            # SHOW QUERY CRITERIA COUNT
-            Write-Verbose -Message ('Query criteria in search: {0}' -f $searchParams)
+            'Owner' {
+                if ($searchParams) { $restParams.Body['q'] += ' AND owner:"{0}"' -f $Owner }
+                else { $restParams.Body['q'] = 'owner:"{0}"' -f $Owner }
+                $searchParams++
+            }
+            'Tag' {
+                if ($searchParams) { $restParams.Body['q'] += ' AND tags:"{0}"' -f $Tag }
+                else { $restParams.Body['q'] = 'tags:"{0}"' -f $Tag }
+                $searchParams++
+            }
+            'Type' {
+                if ($searchParams) { $restParams.Body['q'] += ' AND type:"{0}"' -f $Type }
+                else { $restParams.Body['q'] = 'type:"{0}"' -f $Type }
+                $searchParams++
+            }
+            'Title' {
+                if ($searchParams) { $restParams.Body['q'] += ' AND title:"{0}"' -f $Title }
+                else { $restParams.Body['q'] = 'title:"{0}"' -f $Title }
+                $searchParams++
+            }
         }
+
+        # SHOW QUERY CRITERIA COUNT
+        Write-Verbose -Message ('Query criteria in search: {0}' -f $searchParams)
 
         # SHOW QUERY STRING
         Write-Verbose -Message ('Query string: [{0}]' -f $restParams.Body['q'])
-        Write-Verbose -Message ($restParams.Body | Out-String)
+        #Write-Verbose -Message ($restParams.Body | Out-String)
 
         # ADD CERTIFICATE SKIP IF PROVIDED
         if ($PSBoundParameters.ContainsKey('SkipCertificateCheck')) { $restParams['SkipCertificateCheck'] = $true }
